3b00a31fbc491a827d3fc08c36226d25498143fb
Wolf480pl <wolf480@interia.pl>
2013-12-30 13:36:36 +0100

    Add a format to JULLogger with which it can pass the Marker to the Logger from JUL.

--- src/main/java/com/github/wolf480pl/log4j2_to_jul/context/JULLogger.java
+++ src/main/java/com/github/wolf480pl/log4j2_to_jul/context/JULLogger.java
@@ -12,21 +12,39 @@ import org.apache.logging.log4j.spi.AbstractLogger;
 import com.github.wolf480pl.log4j2_to_jul.Util;
 
 public class JULLogger extends AbstractLogger {
+    public static final String DEFAULT_FORMAT = "%2$s - %1$s";
+    public static final String PASSTHRU_FORMAT = "%s";
     private final Logger jul;
+    private final String format;
 
     public JULLogger(Logger jul) {
+        this(jul, null);
+    }
+
+    public JULLogger(Logger jul, String format) {
         super(jul.getName());
         this.jul = jul;
+        this.format = format;
     }
 
     public JULLogger(String name) {
+        this(name, (String) null);
+    }
+
+    public JULLogger(String name, String format) {
         super(name);
         this.jul = Logger.getLogger(name);
+        this.format = format;
     }
 
     public JULLogger(String name, MessageFactory messageFactory) {
+        this(name, messageFactory, null);
+    }
+
+    public JULLogger(String name, MessageFactory messageFactory, String format) {
         super(name, messageFactory);
         this.jul = Logger.getLogger(name);
+        this.format = format;
     }
 
     @Override
@@ -56,11 +74,21 @@ public class JULLogger extends AbstractLogger {
 
     @Override
     public void log(Marker marker, String fqcn, Level level, Message data, Throwable t) {
-        LogRecord record = new LogRecord(Util.levelToJUL(level), data.getFormattedMessage());
+        final String msg;
+        String mkName = (marker == null) ? "" : marker.getName();
+        if (this.format == null) {
+            if (mkName.isEmpty()) {
+                msg = data.getFormattedMessage();
+            } else {
+                msg = String.format(DEFAULT_FORMAT, mkName, data.getFormattedMessage());
+            }
+        } else {
+            msg = String.format(this.format, mkName, data.getFormattedMessage());
+        }
+        LogRecord record = new LogRecord(Util.levelToJUL(level), msg);
         record.setThrown(t);
         record.setSourceClassName(fqcn);
         record.setLoggerName(this.jul.getName());
-        // TODO: Propagate the marker
         this.jul.log(record);
     }
 
